413cb2d775bbc0e05fa37fdf8dc05e74e88ee74f
Nick Winters <nick@pogseal.com>
2024-10-04 11:24:42 -0700

    Custom editor block with data fetching

--- app/_custom/blocks/Example.tsx
+++ app/_custom/blocks/Example.tsx
@@ -1,6 +1,11 @@
 import type { ReactNode } from "react";
+import { gql, request as gqlRequest } from "graphql-request";
+
+import useSWR from "swr";
 
 import type { ListElement } from "~/routes/_editor+/core/types";
+import { Loading } from "~/components/Loading";
+import { Servant } from "~/db/payload-custom-types";
 
 type Props = {
    element: ListElement;
@@ -8,5 +13,29 @@ type Props = {
 };
 
 export const ExampleBlock = ({ element, children }: Props) => {
-   return <div className="">{children}</div>;
+   const { data, error, isLoading } = useSWR(
+      gql`
+         query {
+            Servants {
+               docs {
+                  name
+               }
+            }
+         }
+      `,
+      (query: any) => gqlRequest("http://localhost:4000/api/graphql", query),
+   );
+   if (error) return null;
+   if (isLoading) return <Loading />;
+
+   //@ts-ignore
+   const servant = data?.Servants?.docs[0] as Servant;
+
+   return (
+      <div contentEditable={false} className="">
+         <div className="border border-color-sub shadow-1 shadow-color p-3 rounded-lg bg-2-sub">
+            {servant.name}
+         </div>
+      </div>
+   );
 };
--- app/_custom/blocks/index.tsx
+++ app/_custom/blocks/index.tsx
@@ -11,13 +11,10 @@ enum BlockType {
 type CustomComponent = {
    id: string;
    type: BlockType.CustomComponent;
-   stringField: string | null;
    children: [{ text: "" }];
 };
 
 export const CustomBlocks = ({ element, children, attributes }: any) => {
-   const readOnly = useReadOnly();
-
    switch (element.type) {
       case BlockType.CustomComponent: {
          return <ExampleBlock element={element} children={children} />;
@@ -34,21 +31,20 @@ export const CustomBlocks = ({ element, children, attributes }: any) => {
 
 export const CustomBlocksAddConfig = (onSelect: any) => {
    return {
-      // label: "Custom",
-      // items: [
-      //    {
-      //       label: "Sample Component",
-      //       icon: <Icon name="component" size={20} />,
-      //       description: "Sample component description",
-      //       onSelect: () => {
-      //          onSelect({
-      //             id: nanoid(),
-      //             stringField: "test",
-      //             type: BlockType.CustomComponent,
-      //             children: [{ text: "" }],
-      //          });
-      //       },
-      //    },
-      // ],
+      label: "Custom",
+      items: [
+         {
+            label: "Example",
+            icon: <Icon name="component" size={20} />,
+            description: "Example component description",
+            onSelect: () => {
+               onSelect({
+                  id: nanoid(),
+                  type: BlockType.CustomComponent,
+                  children: [{ text: "" }],
+               });
+            },
+         },
+      ],
    };
 };
